fbc35fff32d6c0ffbed24ad1593adee012b8b0df
Sean Dela Cruz <andreadlacrz1@gmail.com>
2021-10-25 14:21:24 +0800

    Crab V1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Crab is also called Alimango! 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React? Click here.
+
         </a>
       </header>
     </div>
