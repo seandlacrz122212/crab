97e0e10dae26784f2acd96adfb6876f5bb62460e
Sean Dela Cruz <andreadlacrz1@gmail.comm>
2022-02-09 00:55:53 +0800

    CRAB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import store from '../src/store'
+import { Provider } from 'react-redux'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
- 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+)
